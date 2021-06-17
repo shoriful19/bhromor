bf0c16cb5e607b7ae2e1127a622f375b0907f835
parvezmahmud <mahmudparvez05@gmail.com>
2021-06-17 21:16:12 +0600

    added header intial files

---
+++ src/components/headerComp/headerComponent.js
@@ -0,0 +1,35 @@
+// Header Component
+
+const HeaderBar = () => {
+	return(
+		<div>
+			<div className="inline">
+				Logo here
+			</div>
+			<div className="inline">
+				Home
+			</div>
+			<div className="inline">
+				Blog
+			</div>
+			<div className="inline">
+				Services
+			</div>
+			<div className="inline">
+				Career
+			</div>
+			<div className="inline">
+				Contact
+			</div>
+			<div className="inline">
+				Start reselling
+			</div>
+			<small className="inline">fb logo</small>
+			<small className="inline">insta logo</small>
+			<small className="inline">twitter logo</small>
+		</div>
+	)
+	
+}
+
+export default HeaderBar;

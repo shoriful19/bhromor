db8ca1fa70a9d496ef81950dffe0cfc40cb56c60
parvezmahmud <mahmudparvez05@gmail.com>
2021-06-18 23:46:34 +0600

    moved card component to sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // Main appp
 
 import HeaderBar from './components/headerComp/headerComponent';
+import HeaderBottomSection from './components/frontPage/sections/hdrBottomsec'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App hidden lg:block">
       <HeaderBar />
+      <HeaderBottomSection />
     </div>
   );
 }

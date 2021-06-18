fdf34c5296a0f7e73edfcccfd06ac8726732c686
parvezmahmud <mahmudparvez05@gmail.com>
2021-06-18 23:49:37 +0600

    added sections for front page

---
+++ src/components/frontPage/sections/hdrBottomsec.js
@@ -0,0 +1,36 @@
+// Header Bottom Section
+import CardComp from '../cardsComp/cardComponent';
+import MidTopComp from './midTopSec';
+
+const HeaderBottomSection = () => {
+	return(
+		<div>
+			<div className="bg-green-400 pt-4">
+				<div className="flex">
+					<div className="flex-grow pt-10 text-center">
+						<p className="text-4xl tracking-wider">SELL OUR PRODUCTS</p>
+						<p className="text-4xl tracking-wider">TAKE THE PROFIT</p>
+						<small>Sell our products at your own shop</small>
+					</div>
+					<div className="flex-none">
+						<img style={imgCss} src="https://images.unsplash.com/photo-1573496130488-f3bd89d03653?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+					</div>
+				</div>
+			</div>
+		<CardComp/>
+		<MidTopComp />
+		</div>
+		)
+}
+
+const imgCss = {
+	position:"relative",
+	height:"350px",
+	width:"350px",
+	borderBottomLeftRadius: "500% 400px",
+	borderTopLeftRadius: "500% 400px",
+	borderTopRightRadius: "200px",
+  	borderBottomRightRadius: 0,
+}
+
+export default HeaderBottomSection;
